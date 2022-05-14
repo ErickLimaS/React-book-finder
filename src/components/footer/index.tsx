e3ab066fce3c6ac813176a2f196c594cd4245f3d
Erick <69987890+ErickLimaS@users.noreply.github.com>
2022-05-14 20:29:33 -0300

    Update index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,6 +11,7 @@ export const Footer = () => {
                 </ul>
             </div>
             <div>
+                <span>Site para o <a href='https://erick-lima.netlify.app/' target='_blank' rel='noreferrer'>Meu Portfólio</a>.</span>
                 Photo by <a href="https://unsplash.com/@thoughtcatalog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target='_blank' rel='noreferrer'>Thought Catalog</a> on <a href="https://unsplash.com/s/photos/coffee-book?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target='_blank' rel='noreferrer'>Unsplash</a>
             </div>
 
